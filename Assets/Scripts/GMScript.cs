0ea800dcbfa845022551db6be24d33ca0aefef36
TeamN64 <110045405+TeamN64@users.noreply.github.com>
2025-04-06 12:58:39 -0600

    Modificacion GM y Player sistema de turnos

--- Assets/Scripts/GMScript.cs
+++ Assets/Scripts/GMScript.cs
@@ -1,12 +1,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState { START, PLAYER1TURN, PLAYER2TURN, WON, LOST }
+
 public class GMScript : MonoBehaviour
 {
     public List<Player> players = new List<Player>();
+    public BattleState state;
 
     public void AddPlayer(Player _player)
     {
         players.Add(_player);
+    }
+
+    void Start()
+    {
+        state = BattleState.START;
+        StartBattle();
+    }
+
+    void StartBattle()
+    {
+        Player player1 = players[0];
+        Player player2 = players[1];
+
+        state = BattleState.PLAYER1TURN;
+        Player1Turn(player1, player2);
+    }
+
+    void Player1Turn(Player player1, Player player2)
+    {
+        Debug.Log("Jugador 1 ataca");
+        bool isDead = player2.TakeDamage(player1.damage);
+
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYER2TURN;
+            Player2Turn(player1, player2);
+        }
+    }
+
+    void Player2Turn(Player player1, Player player2)
+    {
+        Debug.Log("Jugador 2 ataca");
+        bool isDead = player1.TakeDamage(player2.damage);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYER1TURN;
+            Player1Turn(player1, player2);
+        }
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+            Debug.Log("¡Jugador 1 gana!");
+        else if (state == BattleState.LOST)
+            Debug.Log("¡Jugador 2 gana!");
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,39 @@ public class Player : MonoBehaviour
 {
     public GMScript gameManager;
     public int health = 100;
-    public string[] moveList = {"Fireball", "Tail Swipe"};
+    public int maxHealth = 100;
+    public string[] moveList = { "Fireball", "Tail Swipe" };
+    public int damage = 20;
 
     void Start()
     {
         gameManager = FindAnyObjectByType<GMScript>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool TakeDamage(int damageAmount)
     {
-        
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            health = 0;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Attack(Player target)
+    {
+        bool isDead = target.TakeDamage(damage);
+        if (isDead)
+        {
+            Debug.Log(target.name + " ha muerto.");
+        }
+        else
+        {
+            Debug.Log(target.name + " tiene " + target.health + " de salud.");
+        }
     }
 
     public void PlayerDetected()
