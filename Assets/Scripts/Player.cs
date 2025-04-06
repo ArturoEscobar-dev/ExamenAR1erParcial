16bebac268a85e6f8ad48cad411b5cec625393dc
iFrames7 <ivanladino576@gmail.com>
2025-04-06 13:14:11 -0600

    Test de merge

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Manager")]
     public GMScript gameManager;
+<<<<<<< Updated upstream
     public int health = 100;
     public int maxHealth = 100;
     public string[] moveList = { "Fireball", "Tail Swipe" };
     public int damage = 20;
+=======
+
+    [Header("Pokemon Data")]
+    public string pkmName;
+    public int pkmMaxHP = 100;
+
+    [Header("Moves and Attacks")]
+    public string[] moveNames = new string[4];
+    public int[] moveDamage = new int[4];
+
+    [HideInInspector] public bool isTurn;
+    [HideInInspector] public bool isDead;
+    [HideInInspector] public int pkmCurrentHP;
+>>>>>>> Stashed changes
 
     void Start()
     {
         gameManager = FindAnyObjectByType<GMScript>();
+        pkmCurrentHP = pkmMaxHP;
+        isTurn = false;
+        isDead = false;
     }
 
+<<<<<<< Updated upstream
     public bool TakeDamage(int damageAmount)
     {
         health -= damageAmount;
@@ -36,11 +57,38 @@ public class Player : MonoBehaviour
         else
         {
             Debug.Log(target.name + " tiene " + target.health + " de salud.");
+=======
+    public void TakeDamage(int _damage)
+    {
+        pkmCurrentHP -= _damage;
+        Debug.Log(pkmCurrentHP.ToString());
+        if (pkmCurrentHP <= 0)
+        {
+            isDead = true;
+            PlayerLost();
+>>>>>>> Stashed changes
         }
     }
 
     public void PlayerDetected()
     {
-        gameManager.AddPlayer(this);
+        if (!isDead)
+        {
+            gameManager.AddPlayer(this);
+            Debug.Log(pkmName + " entered");
+            RenderPlayer(true);
+        }
+    }
+
+    public void PlayerLost()
+    {
+        gameManager.RemovePlayer(this);
+        Debug.Log(pkmName + " exited");
+        RenderPlayer(false);
+    }
+
+    public void RenderPlayer(bool _active)
+    {
+        GetComponent<Renderer>().enabled = _active;
     }
 }
